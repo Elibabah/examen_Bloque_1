3ce78ca40b6211dc52f27cc100b74e14cebca804
Elibabah <delias16.hernandez@gmail.com>
2021-11-11 10:41:47 -0600

    iterando data con función bootcampData

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 fetch("./json.json")
     .then((response) => response.json()) //traer objeto y pasarlo a json
-    .then((data) => console.log(data)) // traer el data para entonces iterarlo
-    .catch((error) => console.log(error)) //cachar error y dar respuesta
+    .then((data) => bootcampData(data)) // traer el data para entonces iterarlo
+
+.catch((error) => console.log(error)) //cachar error y dar respuesta
+
+
+let bootcampData = (data) => {
+    console.log(data)
+}
