0e0b3a0c87ea7bd530e318d5b8e393a4a729e235
Elibabah <delias16.hernandez@gmail.com>
2021-11-11 11:15:20 -0600

    imprimiendo nombres dinámicamente desde json

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 fetch("./json.json")
     .then((response) => response.json()) //traer objeto y pasarlo a json
     .then((data) => bootcampData(data)) // traer el data para entonces iterarlo
-
-.catch((error) => console.log(error)) //cachar error y dar respuesta
+    //.catch((error) => console.log(error)) //cachar error y dar respuesta
 
 
 let bootcampData = (data) => {
-    console.log(data)
+    console.log(data.ourBootcamp)
+    for (const friends of data.ourBootcamp) {
+        console.log(friends)
+        console.log(friends.name)
+        document.getElementById("BootcampMembers").innerHTML += friends.name
+    }
 }
