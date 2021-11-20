bcb20935d71744c3387443f5946de4579d59cd06
Elibabah <delias16.hernandez@gmail.com>
2021-11-19 19:05:17 -0600

    hacker edition

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 fetch("./json.json")
     .then((response) => response.json()) //traer objeto y pasarlo a json
     .then((data) => bootcampData(data)) // traer el data para entonces iterarlo
-    //.catch((error) => console.log(error)) //cachar error y dar respuesta
+    .catch((error) => console.log(error)) //cachar error y dar respuesta
 
 
 //Función para iterar datos del JSON
@@ -11,15 +11,32 @@ let bootcampData = (data) => {
         console.log(friends)
         console.log(friends.name)
         console.log(friends.birthday)
+    }
+    imprimir(data.ourBootcamp) // sacar ourBotcamp
+};
 
-        //Pintar dinámicamente en HTML
 
-        //Integrantes Bootcamp
-        let myMates = document.getElementById("BootcampMembers")
+let imprimir = (we) => {
+
+    //Variables Nombre Cumpleaños
+    let myMates = document.getElementById("BootcampMembers")
+    let ourBirthdays = document.getElementById("cumpleaños")
+
+    //Iterar "friends" para imprimir
+    for (friends of we) {
         myMates.innerHTML += friends.name + " "
-
-        //Fechas de cumpleaños
-        let ourBirthdays = document.getElementById("cumpleaños")
         ourBirthdays.innerHTML += `<li>${friends.birthday}<li>`
     }
 }
+
+
+
+/*let bootcampData = (friends) => {
+    //Integrantes Bootcamp
+    let myMates = document.getElementById("BootcampMembers")
+    myMates.innerHTML += friends.name + " "
+
+    //Fechas de cumpleaños
+    let ourBirthdays = document.getElementById("cumpleaños")
+    ourBirthdays.innerHTML += `<li>${friends.birthday}<li>`
+}*/
